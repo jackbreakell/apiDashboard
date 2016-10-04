1520f8aac4524b7e2308d31d01eaf88004dd422a
Jack Breakell <Jack@Admins-MacBook-Pro-2.local>
2016-10-04 14:17:28 +0100

    Add copyright to footer

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,49 +2,49 @@ import Ember from 'ember';
 
 let apis = [{
     name: "api1",
-    hits: 10,
+    hits: 111,
     success: 9,
     error: 1,
     errorRate: 10
   }, {
     name: "api2",
-    hits: 10,
+    hits: 103,
     success: 9,
     error: 1,
     errorRate: 10
   }, {
     name: "api3",
-    hits: 10,
+    hits: 102,
     success: 9,
     error: 1,
     errorRate: 10
   }, {
     name: "api4",
-    hits: 10,
+    hits: 132,
     success: 9,
     error: 1,
     errorRate: 10
   }, {
     name: "api5",
-    hits: 10,
+    hits: 98,
     success: 9,
     error: 1,
     errorRate: 10
   }, {
     name: "api6",
-    hits: 10,
+    hits: 100,
     success: 9,
     error: 1,
     errorRate: 10
   }, {
     name: "api7",
-    hits: 10,
+    hits: 140,
     success: 9,
     error: 1,
     errorRate: 10
   }, {
     name: "api8",
-    hits: 10,
+    hits: 150,
     success: 9,
     error: 1,
     errorRate: 10
